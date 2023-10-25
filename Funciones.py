d303716031866957a221a81b9d13ae7da56d7468
Luis Fernando Torres Pineda <115508165+Luis-Analyt-IA@users.noreply.github.com>
2023-10-24 19:31:57 -0600

    Update Funciones.py
    Se agrega el dataframe a la funcion de estructura

--- Funciones.py
+++ Funciones.py
@@ -1,10 +1,12 @@
 # Listas
-
 [1,2,3]
 
 # Tupla
  (1,2,3)
 
 # Diccionarios
-
 {'P':1,'S':2}
+
+# DataFrame
+pandas.DataFrame({'ID':range(1,13),
+                  'Mes':['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']})
